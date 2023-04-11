a0b72126b96dea640c789fca363cef723ba2872b
Ihor Arkushyn <snapscan68@gmail.com>
2023-04-11 13:14:32 +0300

    add task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
 
+const startBtn = document.querySelector('[data-start]')
+const stopBtn = document.querySelector('[data-stop]')
+startBtn.addEventListener("click", onStartChange);
+
+function onStartChange() {
+    setInterval(() => {let hexColor = getRandomHexColor();
+    document.body.style.backgroundColor = hexColor;}, 1000)
+}
+if(onStartChange()) {
+    startBtn.style
+}
+// changeColorBtn.addEventListener("click", () => {
+//   let hexColor = getRandomHexColor();
+//   document.body.style.backgroundColor = hexColor;
+//   colorName.textContent = hexColor;
+// });
+// const onClick = () => {
+//   setTimeout(() => {
+//     alert("I love async JS!");
+//   }, 2000);
+// };
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
