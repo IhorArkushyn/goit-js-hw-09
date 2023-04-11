50846698b97b78332ff93422f37067918d65ca90
IhorArkushyn <114363817+IhorArkushyn@users.noreply.github.com>
2023-04-11 10:15:07 +0000

    Deploying to gh-pages from @ IhorArkushyn/goit-js-hw-09@a0b72126b96dea640c789fca363cef723ba2872b 🚀

---
+++ 01-color-switcher.058fd79d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]");document.querySelector("[data-stop]");function o(){setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));document.body.style.backgroundColor=t}),1e3)}t.addEventListener("click",o),o()&&t.style}();
+//# sourceMappingURL=01-color-switcher.058fd79d.js.map
---
+++ 01-color-switcher.e8e9df52.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]");document.querySelector("[data-stop]");function e(){setInterval((()=>{let t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}),1e3)}t.addEventListener("click",e),e()&&t.style;
+//# sourceMappingURL=01-color-switcher.e8e9df52.js.map
